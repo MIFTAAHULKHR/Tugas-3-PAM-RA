e4f7f4cdeb925ba804a8de86bbf7fdeccd676e9e
MIFTAAHULKHR <miftahul.123140064@student.itera.ac.id>
2026-04-11 15:53:02 +0700

    Clear repository

--- composeApp/src/commonMain/kotlin/org/tugas3/project/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.tugas3.project
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/org/tugas3/project/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.tugas3.project
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
